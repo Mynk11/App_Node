c7a1adfc5f9d5e5bde66142f57cd8296532d651c
{mayank} <{mayankguptamg.11@gmail.com}>
2018-11-16 10:21:57 -0800

    init

---
+++ config1.js
@@ -0,0 +1,38 @@
+/*
+Create and export configuration variables
+
+*/
+
+//Containers for all the environments
+//staging { default environment }
+const yargs = require('yargs');
+var argv = yargs.argv;
+console.log("Argv is :", argv);
+console.log();
+var environments = {};
+
+
+environments.staging = {
+    'httpPort': 3000,
+    'httpsPort': 3001,
+    'envName': 'staging'
+};
+
+environments.production = {
+    'httpPort': 5000,
+    'httpsPort': 5001,
+    'envName': 'production'
+
+};
+
+//Determine which environment was passed as a command line arguement
+
+console.log("process.env.NODE_ENV is :" + argv._[0], argv._[1]);
+console.log("process.env.NODE_ENV is using process.argv:" + process.argv[0], process.argv[1]);
+var currentEnvironment = argv._[0] === 'production' ? argv._[0].toLowerCase() : 'staging';
+
+//check that the current environment is one of the environments above if not default to staging
+
+var environmentToExport = typeof (environments[currentEnvironment] == 'object') ? environments[currentEnvironment] : '';
+console.log("environmentToExport is :" + environmentToExport);
+module.exports = environmentToExport;
---
+++ index.js
@@ -0,0 +1,135 @@
+//Dependency
+var config1 = require("./config1");
+var http = require("http");
+var https = require('https');
+var url = require("url");
+var fs = require("fs");
+//var _data = require('./lib/data');
+console.log("config is :" + config1);
+var StringnDecoder = require('string_decoder').StringDecoder;
+//the server should respond to all request with a string
+
+
+//rm -rf .git*
+
+
+//Testing
+//@todo delete this
+
+// _data.delete('test', 'newFile', function (err) {
+//   console.log("this was the error", err);
+// });
+
+//Instantiating Server
+var httpServer = http.createServer(function (req, res) {
+  unifiedServer(req, res);
+});
+
+//Start the server and have it listen on port 3000
+httpServer.listen(config1.httpPort, function () {
+  console.log("the server is listening on " + config1.httpPort + "in " + config1.envName + " mode");
+});
+
+
+var httpsServerOptions = {
+  'key': fs.readFileSync("./https/key.pem"),
+  'cert': fs.readFileSync("./https/cert.pem")
+
+};
+var httpsServer = https.createServer(httpsServerOptions, function (req, res) {
+  unifiedServer(req, res);
+});
+
+//Start the server and have it listen on port 3000
+httpsServer.listen(config1.httpsPort, function () {
+  console.log("the server is listening on " + config1.httpsPort + "in " + config1.envName + " mode");
+});
+
+//  all the server logic for both the http and https server
+
+var unifiedServer = function (req, res) {
+  //get the url and parse it
+
+  var parsedUrl = url.parse(req.url, true);
+  //this true allow the query to be passed further
+  //get the path
+
+  var path = parsedUrl.pathname;
+  console.log("Full path is:" + path);
+
+  var trimmedPath = path.replace(/^\/+|\/+$/g, "");
+  console.log(`Request on path is ${trimmedPath}`);
+  //send the response
+
+  //get the query string as an object
+
+  var queryStringObject = parsedUrl.query;
+  console.log(queryStringObject);
+
+  //get the http method
+
+  var method = req.method.toLocaleLowerCase();
+  console.log(`Request on path is ${trimmedPath} with this method ${method} and query is :`, queryStringObject);
+  //get the header as an object
+  var headers = req.headers;
+  console.log("the requested header is ", headers);
+  //get the apyload if any
+  var decoder = new StringnDecoder('utf-8');
+  var buffer = '';
+  req.on('data', function (data) {
+    buffer += decoder.write(data);
+  });
+  req.on('end', function () {
+    buffer += decoder.end();
+    //choose the handler this request should go to if one is not found use the not dound handler
+    //if the requested path is in the router 
+
+    var chosenHandler = typeof (router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
+    //Construct the data object and send to the handler
+    var data = {
+      'trimmedPath': trimmedPath,
+      'queryStringObject': queryStringObject,
+      'method': method,
+      'headers': headers,
+      'payload': buffer
+
+    };
+    //Route the request to the chosen handler
+
+    chosenHandler(data, function (statusCode, payload) {
+      //use the status code callback by the handler or default 200
+      //use the payload callback by the handler or default to an empty object
+      statusCode = typeof (statusCode) == 'number' ? statusCode : 200;
+      payload = typeof (payload) == 'object' ? payload : {};
+      //convert the payload to a string
+      var payloadString = JSON.stringify(payload);
+
+
+
+      res.setHeader('Content-Type', 'application/json');
+      res.writeHead(statusCode);
+      res.end(payloadString);
+
+      console.log('Returning this response', statusCode, payloadString);
+
+    });
+
+  });
+};
+
+
+
+//define a request router
+var handlers = {};
+handlers.ping = function (data, callback) {
+  //callback a http status code, and a payload object
+  callback(200);
+};
+
+handlers.notFound = function (data, callback) {
+  callback(404);
+};
+
+var router = {
+  'ping': handlers.ping
+};
